dc3bdfd38795b2b8d13ba5684a93a817ef5b217d
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-09-20 10:57:19 -0400

    Added initial project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  // You need this for every monobehavior
+
+/// <summary>
+/// A Hello World script with some basic execution order stuff
+/// 
+/// Check out the docs here: 
+/// https://docs.unity3d.com/Manual/ExecutionOrder.html
+/// </summary>
+public class HelloWorld : MonoBehaviour
+{
+
+    #region Fields
+
+    // Private fields don't show up in the editor!
+    private float TestFoat = 0.0f;
+
+    // Public floats do! 
+    public float TestPublicFloat = 10.0f;
+
+    // If you want to FORCE a private var to show up in the editor, you can use ...
+    [SerializeField]
+    private float PrivateSerializedFloat = 15.0f;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        // Awake happens first
+        TestFoat += 2.0f;
+    }
+
+    private void OnEnable()
+    {
+        // Enable happens next
+        // here you can hook into some events or delegates, stuff like that
+        PrivateSerializedFloat += 10.0f;
+    }
+
+    
+    private void Start ()
+    {
+        // Use this for initialization
+        Debug.Log("Hello world!");
+        TestPublicFloat -= 2.0f;
+
+        // Example of getting a component
+        // Animator anim = GetComponent<Animator>();
+    }
+
+    private void Update ()
+    {
+		// Update game logic here! Called once per frame
+	}
+
+    private void FixedUpdate()
+    {
+        // Put physics things in this update loop!
+    }
+
+    private void LateUpdate()
+    {
+        // This happens after update, you can edit things like camera movement here 
+        // if it is dependant on where other objects are
+    }
+
+    private void OnDisable()
+    {
+        // On disable is where you can remove any event / delegate hooks
+    }
+
+    private void OnDestroy()
+    {
+        // last thing to happen to this game obejct
+    }
+
+    #endregion
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # unity-2d-template
-This is a template project that is set up with our Git Ignore properly for students to use as a template. 
+
+This is a template project that is set up with our Git Ignore properly for
+students to use as a template.
+
+
+## Unity 2018.2.1f1
+
+Remember, it is important that you and your team members use the same Unity
+version to avoid conflicts!
